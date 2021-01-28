9cd817817336cf15c5dafcf566e3c4cf464a81e8
Harshit Kumar <15359962+harshitkumar31@users.noreply.github.com>
2021-01-28 15:35:40 +0530

    Dummy components demo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import React, { Component } from 'react';
 import "./styles.css";
+
+import TodoList from './todoList';
+
+const list = [
+  {
+    name: "FDP",
+    status: false
+  },
+  {
+    name: "Training",
+    status: false
+  }
+];
 
 export default function App() {
   return (
     <div className="App">
       <h1>Todo app</h1>
+      <TodoList list={list} />
+
+      <TodoList list={list} />
+
+      <TodoList list={list} />
     </div>
   );
 }
--- src/todoList.jsx
+++ src/todoList.jsx
@@ -1,8 +1,26 @@
 import React from "react";
 
+import './todolist.css';
+
 /**
  * Presentational Component
  */
 const TodoList = (props) => {
-  return <div></div>;
+
+  const { list } = props;
+  return (<div className="todolist">
+    Todo List
+    {
+      list.map((listItem) => (
+      <div>
+      <input type="checkbox" checked={listItem.status} ></input>
+      <h3>
+        {listItem.name}
+      </h3>
+      </div>
+      ) )
+    }
+    </div>);
 };
+
+export default TodoList;
